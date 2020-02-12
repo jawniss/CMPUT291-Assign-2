76fa9927a9af761c3e040f920ada22e72036abd2
jawniss <johnas@ualberta.ca>
2020-02-12 14:35:21 -0700

    Done 1 and 2

---
+++ 1.sql
@@ -0,0 +1,8 @@
+
+
+SELECT U.email, U.name
+FROM users U, sales S
+WHERE S.sid=B.sid
+AND S.lister=B.bidder
+AND S.lister=U.email
+AND B.bidder=U.email;
---
+++ 2.sql
@@ -0,0 +1,12 @@
+
+
+SELECT S.sid, S.lister, S.cond, S.rprice
+FROM sales S, bids B, users U
+WHERE S.descr LIKE '%ticket%'
+AND S.sid=B.sid
+AND B.bidder=U.email
+AND U.city LIKE '%Edmonton%'
+OR S.descr LIKE '%voucher%'
+AND S.sid=B.sid
+AND B.bidder=U.email
+AND U.city LIKE '%Edmonton%';
