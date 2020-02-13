8ee5381e22d876dd27561591d26a0f5d8255b890
jawniss <johnas@ualberta.ca>
2020-02-13 13:05:53 -0700

    Q1-6 done or half done, Q8 done, Q7, Q9, Q10 still needed

--- 7.sql
+++ 7.sql
@@ -7,3 +7,12 @@ AND bids.amount>=sales.rprice) )
 -- SUM( (WINNING BIDS) )
 FROM users U, sales, bids
 WHERE DATE(sales.edate) < DATE('now');
+
+
+
+
+-- the 'subqueriescan be used in the from clause' maybe
+-- it's like select things from (select things from smoehting
+-- else)
+-- so like only select these things from this selection from
+-- these other things
---
+++ 8.sql
@@ -0,0 +1,10 @@
+SELECT users.email,
+  AVG(reviews.rating)
+FROM users
+LEFT OUTER JOIN reviews ON users.email=reviews.reviewee
+GROUP BY users.name
+HAVING COUNT(users.email=reviews.reviewee) >= 3
+ORDER BY AVG(reviews.rating) DESC
+LIMIT 5;
+
+-- I think this is working
